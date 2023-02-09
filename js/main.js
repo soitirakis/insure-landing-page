36be748b036d2bdba223fc313edd83103d74b9cc
Andrei <zbu_04@yahoo.com>
2023-02-09 23:19:40 +0100

    span close removed; close icon added instead

--- js/main.js
+++ js/main.js
@@ -5,13 +5,31 @@ const hamburger = document.querySelector("#nav-menu");
 
 const close = document.getElementsByClassName('close')[0];
 
+let value = false;
+
 hamburger.addEventListener('click', () => {
+  if(value === false) {
+    modal.style.width = "100%";
+    hamburger.setAttribute("src", "img/icon-close.svg");
+    value = true;
+  }else {
+    modal.style.width = "0";
+    hamburger.setAttribute("src", "img/icon-hamburger.svg");
+    value = false;
+  }
+
+});
+//initialy with the span=close
+/*
+hamburger.addEventListener("click", () => {
   modal.style.width = "100%";
 });
 
 close.addEventListener('click', () => {
   modal.style.width = "0";
+  hamburger.setAttribute("src", "img/icon-hamburger.svg");
 });
+*/
 
 window.addEventListener('click', (e) => {
   if(e.target == modal) {
