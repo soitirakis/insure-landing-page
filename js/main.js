c1bc95e44d48bb3fd97faffddc4d51a3c38d91cc
Andrei <zbu_04@yahoo.com>
2023-02-09 22:42:15 +0100

    mobile first design completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 "use strict"
+const modal = document.querySelector("#myModal");
+
+const hamburger = document.querySelector("#nav-menu");
+
+const close = document.getElementsByClassName('close')[0];
+
+hamburger.addEventListener('click', () => {
+  modal.style.width = "100%";
+});
+
+close.addEventListener('click', () => {
+  modal.style.width = "0";
+});
+
+window.addEventListener('click', (e) => {
+  if(e.target == modal) {
+    modal.style.width = "0";
+  }
+})
